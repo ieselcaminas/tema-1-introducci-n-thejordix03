73b588992c7688b1f32e8a611d3f4e4325c77e16
Jordi Porcar Muñoz <jorpormun@alu.edu.gva.es>
2024-09-20 11:44:23 +0200

    ES EL EJERCICIO 22

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
 public class Prueba {
     public static void main(String[] args) {
 
-        System.out.println("Hello world");
+        int num;
+        int positives = 0;
+
+        Scanner scan = new Scanner(System.in);
+
+        for (int i = 0; i < 10; i++) {
+            num = scan.nextInt();
+
+            if (num > 0) {
+                positives ++;
+            }
+        }
+        System.out.println("positivos : " +positives);
+
+
     }
 }
+
