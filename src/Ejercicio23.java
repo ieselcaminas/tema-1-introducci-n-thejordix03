8889f72bbacb8dc58fb878a89aa5f1e674df72cf
Jordi Porcar Muñoz <jorpormun@alu.edu.gva.es>
2024-09-20 12:13:11 +0200

    Readme

---
+++ src/Ejercicio23.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Ejercicio23 {
+    public static void main(String[] args) {
+
+        int num;
+        int positives = 0;
+
+        Scanner scan = new Scanner(System.in);
+
+        for (int i = 0; i < 10; i++) {
+            num = scan.nextInt();
+
+            if (num > 0) {
+                positives ++;
+            }
+        }
+        System.out.println("positivos : " +positives);
+
+    }
+}
