02f242a3ff45c4dbde28426db8556def3a693179
Jordi Porcar Muñoz <jorpormun@alu.edu.gva.es>
2024-09-26 11:44:01 +0200

    Es el ejercicio 34

---
+++ src/Ejercicio34.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Ejercicio34 {
+    public static void main(String[] args) {
+
+        int num;
+        int num2;
+        int total = 0;
+
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("introduce un numero : ");
+        num = scan.nextInt();
+
+        System.out.println("introduce otro numero : ");
+        num2 = scan.nextInt();
+
+        for (int i = 1;
+             i <= num2;
+             i++) {
+            total = total + num;
+        }
+        System.out.println(total);
+    }
+}
