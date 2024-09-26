608e1080bf41c8cd4b02a0b992c3cc2e47684db5
Jordi Porcar Muñoz <jorpormun@alu.edu.gva.es>
2024-09-26 11:39:37 +0200

    Es el ejercicio 27

---
+++ src/Ejercicio27.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Ejercicio27 {
+    public static void main(String[] args) {
+
+        int n ;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("introduce un numero n : ");
+        n = scan.nextInt();
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.println(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
