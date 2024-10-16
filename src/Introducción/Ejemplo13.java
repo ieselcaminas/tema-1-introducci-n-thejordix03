e5a6ebf2b45ef5b4971d02f80390fbd44e36a778
Jordi Porcar Muñoz <jorpormun@alu.edu.gva.es>
2024-10-16 09:41:57 +0200

    Ejercicios

--- src/Introducción/Ejemplo13.java
+++ src/Introducción/Ejemplo13.java
@@ -1,41 +1,36 @@
 package Introducción;//13 Realiza un programa que lea dos valores y los orden ascendente o descendentemente según elija el usuario. (M)
 import java.util.Scanner;
-public class    Ejemplo13{
+public class    Ejemplo13 {
     public static void main(String args[]) {
-    
-   //variables
-    int x;
-    int y;
-    int decision;
-    
-    
-    Scanner inputValue = new Scanner(System.in);   
-   
-   //pide y lee los valores
-    System.out.println("Introduce los valores:");        
-       x = inputValue.nextInt();
-       y = inputValue.nextInt();
-       
-   //eleccion
-   System.out.println("Elige en 1 - ascendente o 2 - descendente:");        
-      decision = inputValue.nextInt();
-       
-   //Condicional Si SE CUMPLE pasa esto si no PASA LO OTRO
-    if (decision == 1) {
-    System.out.println(x + " " + y);
-    
-    }else{
-    
-    System.out.println(y + " " + x);
+        Scanner sc = new Scanner(System.in);
+        int num1;
+        int num2;
+        int decision;
+        int temp;
+
+        System.out.print("Ingrese un numero: ");
+        num1 = sc.nextInt();
+        System.out.print("Ingrese un numero: ");
+        num2 = sc.nextInt();
+        System.out.print("Ingresa 1 para que sea ascendente o 0 para descendente: ");
+        decision = sc.nextInt();
+
+        if (decision == 1) {
+            if (num1 > num2) {
+                temp = num1;
+                num1 = num2;
+                num2 = temp;
+            }
+            System.out.println("Números ordenados en orden Ascendente: " + num1 + ", " + num2);
+        }
+        if (decision == 0) {
+            if (num1 < num2) {
+                temp = num1;
+                num1 = num2;
+                num2 = temp;
+            }
+            System.out.println("Números ordenados en orden Descendente: " + num1 + ", " + num2);
+        }
     }
+}
     
-    if (decision == 2) {
-    System.out.println(y + " " + x);
-    
-    }else{
-    
-    System.out.println(x + " " + y);
-    }
-    
-  }
-}  
